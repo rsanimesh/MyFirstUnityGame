4c0276dfe3335ccbec778f0b7d4c687a240c9d9b
Rahul Kumar Sinha <rsanimesh@gmail.com>
2019-06-03 04:01:05 +0530

    create player collosion script

---
+++ Assets/Scripts/PlayerCollosion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerCollosion : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    // OnCollisionEnter is called whenever object 
+    // collides with another rigid body
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            
+        }
+    }
+}
